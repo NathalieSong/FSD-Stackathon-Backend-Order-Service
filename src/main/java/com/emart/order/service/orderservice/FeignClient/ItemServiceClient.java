3909054959a9632fa14bdca7b9cd914bf696a575
Yue Juan Song <syjsong@cn.ibm.com>
2020-05-27 23:59:39 +0800

    Update

--- src/main/java/com/emart/order/service/orderservice/FeignClient/ItemServiceClient.java
+++ src/main/java/com/emart/order/service/orderservice/FeignClient/ItemServiceClient.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "${feign.items.name}", url = "${feign.items.url}")
-@RequestMapping("api/item")
+@FeignClient(name = "emart-item-service")
+@RequestMapping("emartItem")
 public interface ItemServiceClient {
     @GetMapping("/byId")
     public Item getById(@RequestParam("itemId") String itemId);
